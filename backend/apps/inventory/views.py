453df88855228c59927b2ddbf6594af3606337c0
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-06-05 18:59:48 -0500

    Started the framework for the inventory views

---
+++ backend/apps/inventory/views.py
@@ -0,0 +1,57 @@
+from django.contrib.auth import get_user_model
+from django.utils.decorators import method_decorator
+from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+
+from .serializers import ChemicalSerializer, ContainerSerializer
+
+
+class Chemical(ViewSet):
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def list(self, request):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
+
+class Container(ViewSet):
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def list(self, request):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
+
+class Location(ViewSet):
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def list(self, request):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
