352ea6c664cc208510287e2e44745e66faa6fc09
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-06-06 10:42:10 -0500

    Migrated changes and added remaining inventory models to admin

--- backend/apps/inventory/admin.py
+++ backend/apps/inventory/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Chemical, Container, Location
+from .models import Chemical, Container, Location, ChemicalStorageCategories, SDS, LocationTypes, WeightReading, CheckoutEvent
 
 admin.site.register(Chemical)
 admin.site.register(Container)
 admin.site.register(Location)
+admin.site.register(ChemicalStorageCategories)
+admin.site.register(SDS)
+admin.site.register(LocationTypes)
+admin.site.register(WeightReading)
+admin.site.register(CheckoutEvent)
+
