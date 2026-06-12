53cc353c8027139741717a966e225ec5453a7744
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-06-12 15:48:54 -0500

    Added a Theme with defined light and dark modes

---
+++ frontend/src/context/Theme.tsx
@@ -0,0 +1,36 @@
+import { createTheme, ThemeProvider } from "@mui/material";
+import { type PropsWithChildren} from "react";
+
+const theme = createTheme({
+  colorSchemes: {
+    light: {
+        palette: {
+            primary: {
+                main: "#260859"
+            },
+            secondary: {
+                main: "#ab8e44"
+            }
+        }
+    },
+    dark: {
+        palette: {
+            primary: {
+                main: "#9d99c6"
+            },
+            secondary: {
+                main: "#ab8e44"
+            }
+        }
+    }
+  }
+})
+
+export const Theme = ({children}: PropsWithChildren) => {
+
+    return (
+        <ThemeProvider theme={theme}>
+            {children}
+        </ThemeProvider>
+    )
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { CssBaseline } from '@mui/material';
+import { CssBaseline} from '@mui/material';
 import { AuthProvider } from './context/AuthContext';
 import App from './App';
+import { Theme } from './context/Theme';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -12,13 +13,17 @@ const queryClient = new QueryClient({
   },
 });
 
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
-          <CssBaseline />
-          <App />
+          <CssBaseline enableColorScheme />
+            <Theme >
+              <App />
+            </Theme>
         </AuthProvider>
       </QueryClientProvider>
     </BrowserRouter>
