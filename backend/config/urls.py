ba48568fbf85379f36e669a5aafb21cc57dc8661
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-06-17 19:27:57 -0500

    Add inventory management commands and update URLs for inventory API

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("apps.users.urls")),
+    path("inventory/", include("apps.inventory.urls")),
 ]
