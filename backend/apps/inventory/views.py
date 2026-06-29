1c9b89adee91b8b84d2ce081fb1f352e88fdacdd
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-06-29 15:59:51 -0500

    feat: implement container creation with multiple CAS handling and ingredient management

--- backend/apps/inventory/views.py
+++ backend/apps/inventory/views.py
@@ -1,9 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import filters
+from rest_framework import filters, status
 from rest_framework.decorators import action
 from natsort import natsorted
 from rest_framework.response import Response
-from .models import Container, Chemical, Location, ChemicalStorageCategories
+from .models import Container, Chemical, Location, ChemicalStorageCategories, Ingredient, WeightReading
 from .serializers import (
     ContainerSerializer,
     ContainerWriteSerializer,
@@ -12,6 +12,7 @@ from .serializers import (
     ChemicalStorageCategoriesSerializer,
 )
 from django.db.models import Count, Q, F
+from django.db import transaction
 
 
 class ChemicalView(ModelViewSet):
@@ -52,7 +53,73 @@ class ContainerView(ModelViewSet):
         if self.action in ["create", "update", "partial_update", "metadata"]:
             return ContainerWriteSerializer
         return ContainerSerializer
+    
+    @transaction.atomic
+    def create(self, request):
+        user = request.user
+        chemicals = Chemical.objects.all()
 
+        data = request.data
+
+        if data.get("multiple_cas"):      
+            if data.get("mixture_id") != "":
+                chemical = Chemical.objects.get(pk=data.get("mixture_id"))
+            else:
+                mixture_data = {
+                    "name": data.get("mixture_name"),
+                    "cas": data.get("mixture_cas"),
+                    "molecular_weight": data.ge("molecular_weight")
+                }
+                if ChemicalSerializer(data=mixture_data).isValid():                    
+                    chemical = Chemical.objects.create(mixture_data)
+                else:
+                    pass
+            request_chems = data.get("chemicals")
+            for chem in request_chems:
+                serializer = ChemicalSerializer(data=chem)
+                serializer.isValid(raise_exception=True)
+                c, _= chemicals.get_or_create(cas=chem.get("cas"))
+                Ingredient.objects.create(
+                    {
+                        "mixture": chemical,
+                        "ingredient": c.id
+                    }
+                )
+
+                    
+        else:
+            request_chem = data.get("chemicals")[0]
+            chemical, created = Chemical.objects.get_or_create(cas=request_chem.get("cas"))
+            if created:
+                chemical.molecular_weight = request_chem.get("molecular_weight")
+                chemical.name = request_chem.get("name")
+                chemical.storage_category = request_chem.get("storage_category")
+        new_container = {
+            "name" : data.get("name"),
+            "chemical" : chemical.id,
+            "location": data.get("location"),
+            "manufacturer": data.get("manufacturer"),
+            "initial_quantity": data.get("initial_quantity"),
+            "quantity_unit": data.get("quantity_unit"),
+            "product_num": data.get("product_num"),
+            "date_received": data.get("date_received"),
+            "density": data.get("density"),
+            "expiration_date": data.get("expiration_date"),
+            "initial_weight": data.get("initial_weight"),
+            "tare_weight": data.get("tare_weight"),
+        }
+        serializer = ContainerWriteSerializer(data=new_container)
+        serializer.is_valid(raise_exception=True)
+        container = serializer.save()
+        WeightReading.objects.create(
+            **{
+                "container": container,
+                "weight": container.initial_weight,
+                "recorded_by": user
+            }
+        )
+        #TODO: Reset IDs on failed creates
+        return(Response(ContainerSerializer(container).data, status=status.HTTP_201_CREATED))
 
 class LocationView(ModelViewSet):
     queryset = Location.objects.all()
